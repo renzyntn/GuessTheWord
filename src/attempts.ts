85ba1b38eeaa6281b0fd20772bea45e4d306dfa3
Renzy Antonio <renzy.antonio0112@gmail.com>
2025-12-18 17:42:01 +0800

    convert attempts.js to attempts.ts

---
+++ src/attempts.ts
@@ -0,0 +1,47 @@
+export const attempts = [
+    {
+        name: "HTML",
+        backgroundColor: "bg-orange-500",
+        color: "text-zinc-100",
+    },
+    {
+        name: "CSS",
+        backgroundColor: "bg-sky-500",
+        color: "text-zinc-100",
+    },
+    {
+        name: "JavaScript",
+        backgroundColor: "bg-yellow-400",
+        color: "text-black",
+    },
+    {
+        name: "React",
+        backgroundColor: "bg-sky-400",
+        color: "text-black",
+    },
+    {
+        name: "TypeScript",
+        backgroundColor: "bg-sky-600",
+        color: "text-zinc-100",
+    },
+    {
+        name: "Node.js",
+        backgroundColor: "bg-green-600",
+        color: "text-zinc-100",
+    },
+    {
+        name: "Python",
+        backgroundColor: "bg-yellow-600",
+        color: "text-black",
+    },
+    {
+        name: "Ruby",
+        backgroundColor: "bg-red-600",
+        color: "text-zinc-100",
+    },
+    {
+        name: "Assembly",
+        backgroundColor: "bg-sky-800",
+        color: "text-zinc-100",
+    },
+]
